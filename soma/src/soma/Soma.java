ed3cb0ecd0fe2cc14090d47c2c26f57e4eb0690e
Iana Oliveira <ianaoliveiralima10@gmail.com>
2020-11-30 15:57:49 -0300

    SOMA

---
+++ soma/src/soma/Soma.java
@@ -0,0 +1,13 @@
+package soma;
+
+/**
+ *
+ * @author Iana Oliveira
+ */
+public class Soma {
+    public static void main(String[] args) {
+        int soma = 1 + 1; 
+        System.out.print(soma);
+    }
+
+}
